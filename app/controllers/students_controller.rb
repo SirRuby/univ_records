ba2fa8e4618c612897652846356a81e3ca0f741a
SirRuby <staffordharrison@protonmail.com>
2020-04-12 23:42:59 +0700

    Extracted redundancies in code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,16 @@
 class StudentsController < ApplicationController
+before_action :set_student, only: [:show, :edit, :update]
+
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+
   end
 
   def edit
-    @student = Student.find(params[:id])
+
   end
 
   def new
@@ -16,7 +18,7 @@ class StudentsController < ApplicationController
   end
 
   def update
-    @student = Student.find(params[:id])
+
     if @student.update(student_params)
       flash[:notice] = "Profile successfully updated"
       redirect_to @student
@@ -29,13 +31,17 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:success] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
   end
 end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
